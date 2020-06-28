32d94284396aff4da2012c23c4b3eba6daab63cc
鑫花 <7486847+XinHuaLuFang@users.noreply.github.com>
2020-06-28 23:12:47 +0800

    Update storage.js

--- js-utils/storage.js
+++ js-utils/storage.js
@@ -1,1 +1,25 @@
-.keep
+function set(key, val) {
+  if (val === null || val === undefined) {
+    val = ''
+  }
+  localStorage.setItem(btoa(key), btoa(val))
+}
+
+function get(key) {
+  return atob(localStorage.getItem(btoa(key)))
+}
+
+function remove(key) {
+  localStorage.removeItem(btoa(key))
+}
+
+function clear() {
+  localStorage.clear()
+}
+
+export default {
+  get,
+  set,
+  remove,
+  clear
+}
